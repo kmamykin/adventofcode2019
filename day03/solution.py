fe0d1d99c7ed1c7441551adadb0347a03521664d
Kliment Mamykin <klyment.mamykin@gartner.com>
2019-12-04 23:31:45 -0500

    Day 3 part 2

--- day03/solution.py
+++ day03/solution.py
@@ -40,31 +40,63 @@ class Segment:
             return [Point(s1.start.x, s2.start.y)]
         return []
 
-def wire_to_segments(wire: str, starting_point: Point) -> [Segment]:
-    moving_instructions = [(i[0], int(i[1:])) for i in [e.strip() for e in wire.split(',')]]
-    instruction_to_points = {
-        'R': lambda i: Point(i, 0),
-        'L': lambda i: Point(-i, 0),
-        'U': lambda i: Point(0, i),
-        'D': lambda i: Point(0, -i)
-    }
-    points = [instruction_to_points[direction](num) for direction, num in moving_instructions]
+    def contains(self, p: Point) -> bool:
+        norm_self = self.normalized()
+        return (
+            ((norm_self.start.x <= p.x <= norm_self.end.x) and (norm_self.start.y == norm_self.end.y == p.y)) or
+            ((norm_self.start.y <= p.y <= norm_self.end.y) and (norm_self.start.x == norm_self.end.x == p.x))
+        )
 
-    def to_segment_combiner(acc, p):
-        segment_end = Point(acc[1].x+p.x, acc[1].y+p.y)
-        return acc[0] + [Segment(acc[1], segment_end)], segment_end
+    def steps(self) -> int:
+        return manhattan_distance(self.start, self.end)
 
-    return reduce(to_segment_combiner, points, ([], starting_point))[0]
+    def steps_to_point(self, p: Point) -> int:
+        if self.contains(p):
+            return manhattan_distance(self.start, p)
+        else:
+            return None
 
+
+class Path:
+    def __init__(self, segments, starting_point = Point(0,0)):
+        self.segments = segments
+        self.start = starting_point
+
+    @classmethod
+    def from_instructions(cls, wire: str, starting_point: Point):
+        moving_instructions = [(i[0], int(i[1:])) for i in [e.strip() for e in wire.split(',')]]
+        instruction_to_points = {
+            'R': lambda i: Point(i, 0),
+            'L': lambda i: Point(-i, 0),
+            'U': lambda i: Point(0, i),
+            'D': lambda i: Point(0, -i)
+        }
+        points = [instruction_to_points[direction](num) for direction, num in moving_instructions]
+
+        def to_segment_combiner(acc, p):
+            segment_end = Point(acc[1].x + p.x, acc[1].y + p.y)
+            return acc[0] + [Segment(acc[1], segment_end)], segment_end
+
+        segments = reduce(to_segment_combiner, points, ([], starting_point))[0]
+        return Path(segments, starting_point)
+
+    def steps_to_point(self, p: Point):
+        steps = 0
+        for segment in self.segments:
+            if segment.contains(p):
+                return steps + segment.steps_to_point(p)
+            else:
+                steps += segment.steps()
+        return None
 
 def segment_intersection(s1, s2):
     return s1.overlap_points(s2)
 
 
-def find_intersections(segments1: [Segment], segments2: [Segment]) -> [Point]:
+def find_intersections(path1: Path, path2: Path) -> [Point]:
     intersections = []
-    for s1 in segments1:
-        for s2 in segments2:
+    for s1 in path1.segments:
+        for s2 in path2.segments:
             intersections += segment_intersection(s1, s2)
     return intersections
 
@@ -73,12 +105,24 @@ def manhattan_distance(p1: Point, p2: Point) -> int:
     return abs(p2.x - p1.x) + abs(p2.y - p1.y)
 
 
-def min_distance_to_intersection_point(wire1: str, wire2: str) -> int:
-    segments1 = wire_to_segments(wire1, Point(0, 0))
-    segments2 = wire_to_segments(wire2, Point(0, 0))
-    intersections = find_intersections(segments1, segments2)
+def find_intersection_with_min_distance(wire1: str, wire2: str) -> int:
+    path1 = Path.from_instructions(wire1, Point(0, 0))
+    path2 = Path.from_instructions(wire2, Point(0, 0))
+    intersections = find_intersections(path1, path2)
     intersections = [p for p in intersections if not p == Point(0,0)]
     return min([manhattan_distance(Point(0,0), point) for point in intersections])
+
+
+def find_intersection_with_min_number_of_steps(wire1: str, wire2: str) -> (Point, int):
+    path1 = Path.from_instructions(wire1, Point(0, 0))
+    path2 = Path.from_instructions(wire2, Point(0, 0))
+    intersections = find_intersections(path1, path2)
+    intersections = [p for p in intersections if not p == Point(0,0)]
+    steps_to_intersections1 = [path1.steps_to_point(i) for i in intersections]
+    steps_to_intersections2 = [path2.steps_to_point(i) for i in intersections]
+    steps_to_intersections = [d1 + d2 for d1, d2 in zip(steps_to_intersections1, steps_to_intersections2)]
+    min_steps_index = steps_to_intersections.index(min(steps_to_intersections))
+    return intersections[min_steps_index], steps_to_intersections[min_steps_index]
 
 
 test1 = {
@@ -92,16 +136,20 @@ test2 = {
     "distance": 135
 }
 
-assert wire_to_segments("R8", Point(0,0)) == [Segment(Point(0,0), Point(8,0))]
-assert wire_to_segments("R8,U5", Point(0,0)) == [Segment(Point(0,0), Point(8,0)), Segment(Point(8,0), Point(8,5))]
-assert wire_to_segments("R8,U5,L5,D3", Point(0,0)) == [Segment(Point(0,0), Point(8,0)), Segment(Point(8,0), Point(8,5)), Segment(Point(8,5), Point(3,5)), Segment(Point(3,5), Point(3,2))]
+assert Path.from_instructions("R8", Point(0,0)).segments == [Segment(Point(0,0), Point(8,0))]
+assert Path.from_instructions("R8,U5", Point(0,0)).segments == [Segment(Point(0,0), Point(8,0)), Segment(Point(8,0), Point(8,5))]
+assert Path.from_instructions("R8,U5,L5,D3", Point(0,0)).segments == [Segment(Point(0,0), Point(8,0)), Segment(Point(8,0), Point(8,5)), Segment(Point(8,5), Point(3,5)), Segment(Point(3,5), Point(3,2))]
 assert manhattan_distance(Point(0,0), Point(2,3)) == 5
 assert manhattan_distance(Point(-1,-1), Point(2,3)) == 7
 assert segment_intersection(Segment(Point(0,0), Point(0,2)), Segment(Point(1,0), Point(1,2))) == []
 assert segment_intersection(Segment(Point(0,0), Point(0,2)), Segment(Point(-1,1), Point(1,1))) == [Point(0,1)]
-assert min_distance_to_intersection_point(test1["1"], test1["2"]) == test1["distance"]
-assert min_distance_to_intersection_point(test2["1"], test2["2"]) == test2["distance"]
+assert find_intersection_with_min_distance(test1["1"], test1["2"]) == test1["distance"]
+assert find_intersection_with_min_distance(test2["1"], test2["2"]) == test2["distance"]
 
 print(segment_intersection(Segment(Point(0,0), Point(0,5)), Segment(Point(0,7), Point(0,5))))
 lines = open("./day03/input.txt").readlines()
-print(min_distance_to_intersection_point(lines[0], lines[1]))
+print(find_intersection_with_min_distance(lines[0], lines[1]))
+
+assert find_intersection_with_min_number_of_steps("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")[1] == 610
+assert find_intersection_with_min_number_of_steps("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")[1] == 410
+print(find_intersection_with_min_number_of_steps(lines[0], lines[1]))
