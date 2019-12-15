e827aa60f6817b9b00a82a00c9dcd452c2bb4678
Kliment Mamykin <klyment.mamykin@gartner.com>
2019-12-15 10:44:47 -0500

    Copied over day 9

---
+++ day11/solution.py
@@ -0,0 +1,376 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Memory:
+    def __init__(self, initial_memory):
+        self.memory = initial_memory.copy()
+
+    def _extend_to(self, address: int):
+        if address >= len(self.memory):
+            additional_storage = [0] * (address - len(self.memory) + 1)
+            self.memory.extend(additional_storage)
+
+    def __getitem__(self, address: int):
+        self._extend_to(address)
+        return self.memory[address]
+
+    def __setitem__(self, address: int, value: int):
+        self._extend_to(address)
+        self.memory[address] = value
+
+
+class IO:
+    def __init__(self, inputs, outputs = []):
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def read(self):
+        if not self.inputs:
+            raise Exception("IO input buffer is empty when trying to read")
+        return self.inputs.pop(0)
+
+    def write(self, value: int):
+        self.outputs.append(value)
+
+
+@dataclass
+class ProgramState:
+    memory: Memory
+    address: int
+    relative_base: int
+    io: IO
+
+    def advance(self, offset: int):
+        return ProgramState(self.memory, self.address + offset, self.relative_base, self.io)
+
+    def advance_relative_base(self, offset: int):
+        return ProgramState(self.memory, self.address, self.relative_base + offset, self.io)
+
+    def with_address(self, address: int):
+        return ProgramState(self.memory, address, self.relative_base, self.io)
+
+    def address_offset(self, offset: int = 0) -> int:
+        return self.memory[self.address + offset]
+
+
+class ParameterMode(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+    RELATIVE = 2
+
+
+@dataclass
+class Parameter:
+    value: int
+    mode: ParameterMode
+
+    def read(self, state: ProgramState):
+        if self.mode == ParameterMode.POSITION:
+            return state.memory[self.value]
+        elif self.mode == ParameterMode.IMMEDIATE:
+            return self.value
+        elif self.mode == ParameterMode.RELATIVE:
+            return state.memory[state.relative_base + self.value]
+        else:
+            raise Exception(f"Unknown parameter mode {self.mode}")
+
+    def write(self, state: ProgramState, value):
+        if self.mode == ParameterMode.POSITION:
+            state.memory[self.value] = value
+        elif self.mode == ParameterMode.IMMEDIATE:
+            raise Exception("Can not write to memory at the position of immediate parameter")
+        elif self.mode == ParameterMode.RELATIVE:
+            state.memory[state.relative_base + self.value] = value
+        else:
+            raise Exception(f"Unknown parameter mode {self.mode}")
+
+
+@dataclass
+class InstructionDescriptor:
+    value: int
+
+    def opcode(self):
+        return self.value % 100
+
+    def parameter_mode(self, parameter_order: int) -> ParameterMode:
+        digit_position = parameter_order + 1
+        mode = ((self.value % pow(10, digit_position + 1)) - (self.value % pow(10, digit_position)))//pow(10, digit_position)
+        return ParameterMode(mode)
+
+
+
+@dataclass
+class HaltInstruction:
+
+    @staticmethod
+    def from_memory(state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return HaltInstruction() if descriptor.opcode() == 99 else None
+
+    def execute(self, state: ProgramState):
+        # NOOP
+        return state
+
+
+@dataclass
+class AddInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+    parameter3: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2)),
+            Parameter(state.address_offset(3), descriptor.parameter_mode(3))
+        ) if descriptor.opcode() == 1 else None
+
+    def execute(self, state: ProgramState):
+        result = self.parameter1.read(state) + self.parameter2.read(state)
+        self.parameter3.write(state, result)
+        return state.advance(4)
+
+
+@dataclass
+class MultiplyInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+    parameter3: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2)),
+            Parameter(state.address_offset(3), descriptor.parameter_mode(3))
+        ) if descriptor.opcode() == 2 else None
+
+    def execute(self, state: ProgramState):
+        result = self.parameter1.read(state) * self.parameter2.read(state)
+        self.parameter3.write(state, result)
+        return state.advance(4)
+
+
+@dataclass
+class InputInstruction:
+    parameter: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+        ) if descriptor.opcode() == 3 else None
+
+    def execute(self, state: ProgramState):
+        self.parameter.write(state, state.io.read())
+        return state.advance(2)
+
+
+@dataclass
+class OutputInstruction:
+    parameter: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+        ) if descriptor.opcode() == 4 else None
+
+    def execute(self, state: ProgramState):
+        state.io.write(self.parameter.read(state))
+        return state.advance(2)
+
+
+@dataclass
+class JumpIfTrueInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2))
+        ) if descriptor.opcode() == 5 else None
+
+    def execute(self, state: ProgramState):
+        if self.parameter1.read(state) > 0:
+            return state.with_address(self.parameter2.read(state))
+        else:
+            return state.advance(3)
+
+
+@dataclass
+class JumpIfFalseInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2))
+        ) if descriptor.opcode() == 6 else None
+
+    def execute(self, state: ProgramState):
+        if self.parameter1.read(state) == 0:
+            return state.with_address(self.parameter2.read(state))
+        else:
+            return state.advance(3)
+
+
+@dataclass
+class LessThenInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+    parameter3: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2)),
+            Parameter(state.address_offset(3), descriptor.parameter_mode(3))
+        ) if descriptor.opcode() == 7 else None
+
+    def execute(self, state: ProgramState):
+        if self.parameter1.read(state) < self.parameter2.read(state):
+            self.parameter3.write(state, 1)
+        else:
+            self.parameter3.write(state, 0)
+        return state.advance(4)
+
+
+@dataclass
+class EqualsInstruction:
+    parameter1: Parameter
+    parameter2: Parameter
+    parameter3: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1)),
+            Parameter(state.address_offset(2), descriptor.parameter_mode(2)),
+            Parameter(state.address_offset(3), descriptor.parameter_mode(3))
+        ) if descriptor.opcode() == 8 else None
+
+    def execute(self, state: ProgramState):
+        if self.parameter1.read(state) == self.parameter2.read(state):
+            self.parameter3.write(state, 1)
+        else:
+            self.parameter3.write(state, 0)
+        return state.advance(4)
+
+
+@dataclass
+class RelativeBaseOffsetInstruction:
+    parameter: Parameter
+
+    @classmethod
+    def from_memory(cls, state: ProgramState):
+        descriptor = InstructionDescriptor(state.address_offset())
+        return cls(
+            Parameter(state.address_offset(1), descriptor.parameter_mode(1))
+        ) if descriptor.opcode() == 9 else None
+
+    def execute(self, state: ProgramState):
+        # TODO: do something with relative_base
+        offset = self.parameter.read(state)
+
+        return state.advance(2).advance_relative_base(offset)
+
+
+class IntcodeProgram:
+    state: ProgramState
+
+    def __init__(self, program, inputs, outputs=None):
+        self.state = ProgramState(Memory(program), 0, 0, IO(inputs, [] if outputs is None else outputs))
+        self.halted = False
+        self.instruction_types = [
+            HaltInstruction,
+            AddInstruction, MultiplyInstruction, InputInstruction, OutputInstruction,
+            JumpIfTrueInstruction, JumpIfFalseInstruction, LessThenInstruction, EqualsInstruction,
+            RelativeBaseOffsetInstruction
+        ]
+
+    def next_instruction(self):
+        for type in self.instruction_types:
+            instruction = type.from_memory(self.state)
+            if instruction:
+                return instruction
+        raise Exception(f"Unkown instruction descriptor {self.state.memory[self.state.address]} at position {self.state.address}")
+
+    def execute(self):
+        while not self.halted:
+            instruction = self.next_instruction()
+            self.state = instruction.execute(self.state)
+            self.halted = isinstance(instruction, HaltInstruction)
+        return self.state
+
+    def execute_until_output_or_halt(self):
+        while True:
+            instruction = self.next_instruction()
+            self.address = instruction.execute(self.state)
+            if isinstance(instruction, HaltInstruction):
+                self.halted = True
+                break
+            if isinstance(instruction, OutputInstruction):
+                break
+        return self.state
+
+program = [int(s) for s in open("day11/input1.txt").read().strip().split(',')]
+
+
+# mem = Memory([0,1,2,3,4,5])
+# assert mem[0] == 0
+# assert mem[5] == 5
+# mem[2] = 22
+# assert mem[2] == 22
+# mem[6] = 6
+# assert mem[6] == 6
+# mem[10] = 10
+# assert mem[10] == 10
+#
+# # input equals to 8 program
+# assert IntcodeProgram([3,9,8,9,10,9,4,9,99,-1,8], [8]).execute().io.outputs[-1] == 1
+# assert IntcodeProgram([3,9,8,9,10,9,4,9,99,-1,8], [7]).execute().io.outputs[-1] == 0
+#
+# # input less then 8
+# assert IntcodeProgram([3,9,7,9,10,9,4,9,99,-1,8], [8]).execute().io.outputs[-1] == 0
+# assert IntcodeProgram([3,9,7,9,10,9,4,9,99,-1,8], [7]).execute().io.outputs[-1] == 1
+#
+# # input equal to 8
+# assert IntcodeProgram([3,3,1108,-1,8,3,4,3,99], [8]).execute().io.outputs[-1] == 1
+# assert IntcodeProgram([3,3,1108,-1,8,3,4,3,99], [7]).execute().io.outputs[-1] == 0
+#
+# # input less then 8
+# assert IntcodeProgram([3,3,1107,-1,8,3,4,3,99], [8]).execute().io.outputs[-1] == 0
+# assert IntcodeProgram([3,3,1107,-1,8,3,4,3,99], [7]).execute().io.outputs[-1] == 1
+#
+# # jump tests
+# assert IntcodeProgram([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [0]).execute().io.outputs[-1] == 0
+# assert IntcodeProgram([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [10]).execute().io.outputs[-1] == 1
+# assert IntcodeProgram([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [0]).execute().io.outputs[-1] == 0
+# assert IntcodeProgram([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [10]).execute().io.outputs[-1] == 1
+#
+# # new test cases
+# assert IntcodeProgram([109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], []).execute().io.outputs == [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+# assert IntcodeProgram([1102,34915192,34915192,7,4,7,99,0], []).execute().io.outputs == [1219070632396864]
+# assert IntcodeProgram([104,1125899906842624,99], []).execute().io.outputs == [1125899906842624]
+#
+#
+#
+# print(IntcodeProgram(program, [1]).execute().io.outputs)
+# print(IntcodeProgram(program, [2]).execute().io.outputs)
+#
