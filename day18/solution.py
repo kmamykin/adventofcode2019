9ffab12c22cae01a9396ccc2bc4404a5de020949
Kliment Mamykin <klyment.mamykin@gartner.com>
2019-12-24 13:50:14 -0500

    Day 18 part 2

--- day18/solution.py
+++ day18/solution.py
@@ -95,23 +95,23 @@ def print_map(map):
 
 
 class VaultState:
-    def __init__(self, graph, paths, size, position, keys, doors):
+    def __init__(self, graph, paths, size, positions, keys, doors):
         self.graph = graph
         self.paths = paths
         self.size = size
-        self.position = position
+        self.positions = positions
         self.keys = keys
         self.doors = doors
 
     @property
     def id(self):
-        return f"id-({self.position.x},{self.position.y})-" + "".join([k for k in self.keys.values()])
+        return f"id-{'-'.join([str(p.x)+','+str(p.y) for p in self.positions])}-" + "".join([k for k in self.keys.values()])
 
     @classmethod
     def from_map(cls, map):
         graph = nx.Graph()
         size = map.size
-        position = None
+        positions = []
         keys = {}
         doors = {}
         for y in range(1, map.size.y-1):
@@ -120,7 +120,7 @@ class VaultState:
                 c = map[p]
                 if c != '#':
                     if c == '@':
-                        position = p
+                        positions.append(p)
                     elif 'a' <= c <= 'z':
                         keys[p] = c
                     elif 'A' <= c <= 'Z':
@@ -135,14 +135,25 @@ class VaultState:
                     pass
         paths = []
         for fp, tp in combinations(keys.keys(), 2):
-            for path in nx.all_shortest_paths(graph, fp, tp):
+            shortest_paths = nx.all_shortest_paths(graph, fp, tp)
+            try:
+                shortest_paths = list(shortest_paths)
+            except nx.exception.NetworkXNoPath:
+                shortest_paths = []
+            for path in shortest_paths:
                 paths.append((fp, tp, path))
                 paths.append((tp, fp, path)) # nothing relies on the path pointing one way or another
         for tp in keys.keys():
-            for path in nx.all_shortest_paths(graph, position, tp):
-                paths.append((position, tp, path))
+            for position in positions:
+                shortest_paths = nx.all_shortest_paths(graph, position, tp)
+                try:
+                    shortest_paths = list(shortest_paths)
+                except nx.exception.NetworkXNoPath:
+                    shortest_paths = []
+                for path in shortest_paths:
+                    paths.append((position, tp, path))
 
-        return cls(graph, paths, size, position, keys, doors)
+        return cls(graph, paths, size, positions, keys, doors)
 
     def as_map(self):
         map = Map()
@@ -150,7 +161,8 @@ class VaultState:
         map[self.size+P(-1,-1)] = '#'
         for node in self.graph.nodes():
             map[node] = '.'
-        map[self.position] = '@'
+        for position in self.positions:
+            map[position] = '@'
         for p, c in self.keys.items():
             map[p] = c
         for p, c in self.doors.items():
@@ -158,24 +170,25 @@ class VaultState:
         return map
 
     def possible_moves(self):
-        # find @ as the position
         paths = []
         for fp, tp, path in self.paths:
-            if fp == self.position and tp in self.keys:
-                blocked = False
-                for u in path:
-                    if u in self.doors.keys():
-                        blocked = True
-                if not blocked:
-                    paths.append((tp, len(path)-1))
+            for position in self.positions:
+                if fp == position and tp in self.keys:
+                    blocked = False
+                    for u in path:
+                        if u in self.doors.keys():
+                            blocked = True
+                    if not blocked:
+                        paths.append((position, tp, len(path)-1))
         return paths
 
-    def move_to(self, key_position):
-        key = self.keys[key_position]
+    def move_to(self, position_to_move, new_position):
+        new_positions = [new_position if p==position_to_move else p for p in self.positions]
+        key = self.keys[new_position]
         door = key.upper()
-        new_keys = {p:c for p, c in self.keys.items() if p!= key_position}
+        new_keys = {p:c for p, c in self.keys.items() if p != new_position}
         new_doors = {p:c for p, c in self.doors.items() if c!= door}
-        return VaultState(self.graph, self.paths, self.size, key_position, new_keys, new_doors)
+        return VaultState(self.graph, self.paths, self.size, new_positions, new_keys, new_doors)
 
 
 def explore_moves(state, distances):
@@ -185,8 +198,8 @@ def explore_moves(state, distances):
         distances[state.id] = 0
         return 0
     min_dist = None
-    for end_point, dist_to_point in state.possible_moves():
-        next_state = state.move_to(end_point)
+    for from_point, end_point, dist_to_point in state.possible_moves():
+        next_state = state.move_to(from_point, end_point)
         dist_from_point_to_end = explore_moves(next_state, distances)
         if min_dist is None or (dist_to_point + dist_from_point_to_end) < min_dist:
             min_dist = dist_to_point + dist_from_point_to_end
@@ -207,14 +220,14 @@ def shortest_path_to_collect_all_keys(state):
 # """))
 # assert shortest_path_to_collect_all_keys(s) == 8
 
-s = VaultState.from_map(parse_map("""
-########################
-#f.D.E.e.C.b.A.@.a.B.c.#
-######################.#
-#d.....................#
-########################
-"""))
-assert shortest_path_to_collect_all_keys(s) == 86
+# s = VaultState.from_map(parse_map("""
+# ########################
+# #f.D.E.e.C.b.A.@.a.B.c.#
+# ######################.#
+# #d.....................#
+# ########################
+# """))
+# assert shortest_path_to_collect_all_keys(s) == 86
 
 # s = VaultState.from_map(parse_map("""
 # #################
@@ -230,3 +243,29 @@ assert shortest_path_to_collect_all_keys(s) == 86
 # assert shortest_path_to_collect_all_keys(s) == 136
 # s = VaultState.from_map(parse_map(open("day18/input1.txt").read()))
 # print(shortest_path_to_collect_all_keys(s))
+
+
+# s = VaultState.from_map(parse_map("""
+# ###############
+# #d.ABC.#.....a#
+# ######@#@######
+# ###############
+# ######@#@######
+# #b.....#.....c#
+# ###############
+# """))
+# assert shortest_path_to_collect_all_keys(s) == 24
+
+# s = VaultState.from_map(parse_map("""
+# #############
+# #DcBa.#.GhKl#
+# #.###@#@#I###
+# #e#d#####j#k#
+# ###C#@#@###J#
+# #fEbA.#.FgHi#
+# #############
+# """))
+# assert shortest_path_to_collect_all_keys(s) == 32
+
+s = VaultState.from_map(parse_map(open("day18/input2.txt").read()))
+print(shortest_path_to_collect_all_keys(s))
